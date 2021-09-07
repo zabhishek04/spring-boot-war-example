291aacd2b0c3bb33fdd14604060a327eea94ff7d
coolgourav147 <coolgourav147@gmail.com>
2021-09-07 17:42:52 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "test deploy3";
+        return "test deploy4";
     }
 
 }
